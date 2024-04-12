0fda77a5d70e1536a182137d94acf7ac64e69a6c
Rucy <127585375+rucykun@users.noreply.github.com>
2024-04-12 13:11:55 +0900

    Create stylesheet.js

---
+++ stylesheet.js
@@ -0,0 +1,35 @@
+
+body {
+    background-color: #000001;
+}
+
+#container {
+
+    background-color: #000001;
+    opacity: 1;
+ 
+}
+
+
+#header {
+    text-align: center;
+    margin-top: 47px;
+    margin: auto;
+    font-size: 39px;
+    color: rgb(255, 255, 255)
+
+}
+
+#button {
+    width: 35%;
+    font-size: 25px;
+    color: #838383c9;
+    margin-top: 20px;
+    padding: 15px;
+    background-color: #000000;
+    border: 2px solid #838383c9;
+}
+
+#button:hover { 
+    background-color: #ffffff;
+}
